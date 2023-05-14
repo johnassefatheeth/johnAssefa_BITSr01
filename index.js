00dfc80e8610d146a0caf2d942a7a593689c0f5d
johnassefatheeth <johnassefaforbussines@gmail.com>
2023-05-14 06:02:46 -0700

    responsivenes complete

--- index.js
+++ index.js
@@ -2,23 +2,23 @@
 
 var menu= document.getElementsByClassName("menu")[0];
 
-menu.style.fill='blue';
+menu.style.fill='rgb(0, 255, 17)';
 
 
 
 
 var nav= document.getElementsByTagName("nav")[0];
 
-nav.style.transform = "scale(0)";
+nav.style.transform = "rotateY(90deg)";
 
 let ch=1
 menu.addEventListener("click",function() {
     
     if (ch===1) {
-        nav.style.transform = "scale(1)";
+        nav.style.transform = "rotateY(0deg)";
         ch=0;
     } else {
-        nav.style.transform = "scale(0)";
+        nav.style.transform = "rotateY(90deg)";
         ch=1;
     }
   })
